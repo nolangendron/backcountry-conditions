4fe97c180f8e129d133dbc6c6e634c52666301fd
nolangendron <nolanglengendron@gmail.com>
2020-05-01 13:12:18 -0700

    updates css

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,7 +12,7 @@ import { WindChart } from './WindChart';
 
 
 const Container = styled("main")`
-    padding-top: 81px;
+    padding-top: 80px;
     overflow-y: auto;
     overflow-x: hidden;
     -webkit-overflow-scrolling: touch;
