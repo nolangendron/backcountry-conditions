f7e4975b11b936e98481f4d2676c6a037aaaacd7
nolangendron <nolanglengendron@gmail.com>
2020-06-05 15:31:25 -0700

    Adds styling to snowfallchart

--- src/components/SnowFallChart.js
+++ src/components/SnowFallChart.js
@@ -4,14 +4,39 @@ import { Bar } from "react-chartjs-2";
 
 const SnowFallChartStyles = styled("div")`
   grid-column: 5 / span 1;
+  grid-row: 1 / span 1;
 `;
 const SnowFallChart = ({ data }) => {
   console.log(data);
   return (
     <SnowFallChartStyles>
-      <div className="chart">
-        <Bar data={data} />
-      </div>
+      <Bar
+        data={data}
+        width={"80%"}
+        height={"80%"}
+        options={{
+          legend: {
+            display: false,
+          },
+          scales: {
+            xAxes: [
+              {
+                gridLines: {
+                  display: false,
+                },
+              },
+            ],
+            yAxes: [
+              {
+                display: false,
+                gridLines: {
+                  display: false,
+                },
+              },
+            ],
+          },
+        }}
+      />
     </SnowFallChartStyles>
   );
 };
